8986128a349b5b745d4494ab53f5ed0f742ac4df
rogelioCodes <droozycraft@gmail.com>
2024-05-09 19:02:07 -0700

    added form validation for video uploads. Now display error messages and only accepts valid file types. Changed styling of form page to be more beautiful. Also changed styling of navbar to be more beautiful.

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -40,21 +40,25 @@ export default function Navbar() {
         )
     }
     return (
+
         <nav className={styles.nav}>
-            <Link href="/" >
-                <Image width={90} height={20}
-                    src="/youtube-logo.svg" alt="YoutTube Logo" />
-            </Link>
-            {
-                
-                user && <Fragment>
-                    <Link href="/upload" >
-                    <button>upload</button>
+            <div>
+                <Link href="/" >
+                    <Image width={90} height={20} src="/youtube-logo.svg" alt="YouTube Logo" />
+                </Link>
+            </div>
+            <div className={styles.buttonContainer}>
+                {user && (
+                    <Link href="/upload">
+                        <button className={styles.uploadButton}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                            </svg>
+                        </button>
                     </Link>
-                </Fragment>
-                //<Upload />
-            }
-            <SignIn user={user} />
+                )}
+                <SignIn user={user} />
+            </div>
         </nav>
     );
 }
--- yt-web-client/app/upload/page.tsx
+++ yt-web-client/app/upload/page.tsx
@@ -1,9 +1,15 @@
 'use client';
-import { Fragment, useState } from "react";
-import { DevTool } from "@hookform/devtools";
+import { Fragment, useState, useEffect } from "react";
+// import { DevTool } from "@hookform/devtools";
 import { uploadVideo } from "../utilities/firebase/functions";
 import { useForm, Controller } from 'react-hook-form';
 import styles from "./upload.module.css";
+import dynamic from 'next/dynamic';
+const DevT: React.ElementType = dynamic(
+    () => import('@hookform/devtools').then((module) => module.DevTool),
+    { ssr: false }
+);
+
 // touched means interacted with
 // dirty means value has changed
 type FormValues = {
@@ -12,40 +18,28 @@ type FormValues = {
     file: FileList
 }
 export default function Upload() {
-    // let title;
-    // let description;
     const form = useForm<FormValues>();
-    const { register, control, formState: { errors }, handleSubmit } = form;
-
+    const [fileName, setFileName] = useState<string>("");
+    const { register, control, formState: { errors }, setError, handleSubmit } = form;
     const onSubmit = (data: FormValues) => {
-        
+
         const file = data.file.item(0)
         console.log(`file in form: `, file)
-        
+
         console.log('Form submitted')
         console.log(data)
         if (file) {
             console.log("handling upload!")
             handleUpload(data.title, data.description, file)
-            
+
         }
-        
+
 
 
     }
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.item(0);
 
-        console.log("here")
-        
-        if (file) {
-
-            // handleUpload(file);
-        }
-    }
     const handleUpload = async (title: string, description: string, file: File) => {
-
         try {
             const response = await uploadVideo(title, description, file);
             alert(`File uploaded successfully. Response: ${JSON.stringify(response)}`);
@@ -54,61 +48,93 @@ export default function Upload() {
         }
 
     }
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const allowedTypes = [
+            "video/webm",          // WebM
+            "video/mp4",           // MPEG4, 3GPP, MOV
+            "video/avi",           // AVI
+            "video/mpeg",          // MPEGPS
+            "video/x-ms-wmv",      // WMV
+            "video/x-flv",         // FLV
+            "video/mts",           // MTS
+            "video/ogg"            // OGG
+        ];
 
+        const file = event.target.files?.item(0);
 
+        if (!file || !allowedTypes.includes(file.type)) {
+            // File is not one of the allowed types or no file selected
+            setError("file", {
+                type: "filetype",
+                message: "File format not supported or no file selected."
+            });
+            return;
+        }
+
+        if (file) {
+            console.log("file:", file)
+            setFileName(file.name); // Update file name when a file is selected
+        }
+    }
     return (
 
         <div className={styles.bodyDiv}>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                <div className={styles.form_control}>
+                    <label className={styles.uploadLabel} htmlFor="title">Title</label>
+                    <input className={styles.uploadInput} type="text" id="title" placeholder="Title..." {...register("title", { required: 'Title is required.' })} />
+                    <p className={styles.error} >{errors.title?.message}</p>
+                </div>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label className={styles.uploadLabel} htmlFor="title">{ }</label>
-                <input className={styles.uploadInput} type="text" id="title" placeholder="Title" {...register("title")} />
-
-                <label className={styles.uploadLabel} htmlFor="Description">Description</label>
-                <input className={styles.uploadInput} type="text" id="description" {...register("description")} />
-
-                <input 
-                    {...register("file", {
-                        required: "file is required",
-                    })}
-                    type="file"
-                    id="upload"
-                />
-                    {/* <Controller
-                    control={control}
-                    name={"upload"}
-                    rules={{ required: "file upload is required" }}
-                    render={({ field: { value, onChange, ...field } }) => {
-                        return (
+                <div className={styles.form_control}>
+                    <label className={styles.uploadLabel} htmlFor="Description">Description</label>
+                    <textarea className={`${styles.uploadInput}`} placeholder="Description..." id={styles.description} {...register("description", { required: 'Description is required.' })} maxLength={5000} />
+                    <p className={styles.error}>{errors.description?.message}</p>
+                    
+                </div>
+                <div className={styles.form_control}>
+                    <div className={styles.fileInputContainer}>
+                        <label className={styles.customFileUpload} htmlFor="upload">
+                            <div className={styles.icon}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+                                </svg>
+                            </div>
+                            <div className={styles.text}>
+                                <span>{fileName ? (
+                                    <span className={`${styles.uploadLabel} ${styles.fileSpan}`}>{fileName}</span>
+                                ) : (
+                                    <span className={`${styles.uploadLabel} ${styles.fileSpan}`}>Click to upload file</span>
+                                )}</span>
+                            </div>
                             <input
-                                
-                                {...field}
-                                value={value?.fileName}
-                                onChange={handleFileChange}
+                                className={`${styles.uploadButtonInput}`}
+                                {...register("file", {
+                                    required: "File is required.",
+                                    onChange: (e) => {
+                                        handleFileChange(e);
+                                    }
+                                })}
                                 type="file"
                                 id="upload"
+                                accept="video/*"
                             />
-                        );
-                    }}
-                /> */}
-                {/* <input className={styles.uploadButtonInput} type="file" accept="video/*" id="upload"  {...register("upload", {
-                    required: "File is required"
-                })} /> */}
-                <label htmlFor="upload" className={styles.uploadButton} >
 
-                    {/* {selectedFile ? selectedFile.name : "Choose File"} */}
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
-                    </svg>
-                </label>
+                        </label>
+                        <div>
+                            <p className={styles.error}>{errors.file?.message}</p>
+                        </div>
+                    </div>
+                </div>
 
                 <div className={styles.upload_button_container}>
                     <button className={styles.upload_button}>Submit</button>
                 </div>
-
+                <DevT control={control} />
             </form>
-            <DevTool control={control} />
+
         </div>
+
     )
 
 }
